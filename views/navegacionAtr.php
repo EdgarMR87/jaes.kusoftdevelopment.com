ebb4a80fa703cce38377bbe6571efa454c533a3f
EdgarMR87 <81342053+EdgarMR87@users.noreply.github.com>
2021-04-07 09:33:36 -0500

    07-04-2021 9:33AM

--- views/navegacionAtr.php
+++ views/navegacionAtr.php
@@ -1,5 +1,4 @@
 <script>
-
     function salir(){
         swal({
             title: "¿Deseas Cerrar Sesión?",
@@ -9,13 +8,18 @@
             cancelButtonText: "Cancelar",
             confirmButtonColor: "#DD6B55",
             confirmButtonText: "Continuar",
-            closeOnConfirm: false
+            closeOnClickOutside: false,
+            closeOnEsc: false,
+            allowOutsideClick: false,
         }).then(function(result) {
                 if (result.value) {
                     swal({
                         title: "Adios",
                         text: "Saliendo del sistema .....",
                         type: "success",
+                        closeOnClickOutside: false,
+                        closeOnEsc: false,
+                        allowOutsideClick: false,
                     }).then(function(result) {
                             if(result.value){
                                 location.href = "index.php?action=exit";
@@ -59,13 +63,13 @@
 <div class="submenu-usuarios" id="submenu-os">
 	<div class="listado-submenu">
 		<a href="index.php?action=OrdenesServicio/listadoOS">
-			<img src="/views/img/ListadoUsuarios.png">
+			<img src="/views/img/Listado_OS.png">
 			<p>Listado Ordenes Servicio</p>
 		</a>
 	</div>
 	<div class="listado-submenu">
 		<a href="index.php?action=OrdenesServicio/altaOrdenS">
-			<img src="/views/img/NuevoUsuario.png">
+			<img src="/views/img/Add_OS.png">
 			<p>Agregar Orden Servicio</p>
 		</a>
 	</div>
@@ -89,8 +93,8 @@
 	</div>
     <div class="listado-submenu">
 		<a href="index.php?action=OrdenesServicio/listadoServiciosEnProceso">
-			<img src="/views/img/asignarUsuarios.png">
-			<p>Servicio En Proceso</p>
+			<img src="/views/img/servicios_Proceso.png">
+			<p>Servicios En Proceso</p>
 		</a>
 	</div>
 
@@ -100,13 +104,13 @@
 <div class="submenu-usuarios" id="submenu-unidades">
 	<div class="listado-submenu">
 		<a href="index.php?action=Unidades/listadoUnidades">
-			<img src="/views/img/Unidades.png">
+			<img src="/views/img/Listado_Trailers.png">
 			<p>Listado Unidades</p>
 		</a>
 	</div>
 	<div class="listado-submenu">
 		<a href="index.php?action=Unidades/altaUnidad">
-			<img src="/views/img/Departamentos.png">
+			<img src="/views/img/Add_Trailer.png">
 			<p>Agregar Unidad</p>
 		</a>
 	</div>
@@ -115,13 +119,13 @@
 <div class="submenu-usuarios" id="submenu-servicios">
 	<div class="listado-submenu">
 		<a href="index.php?action=ServiciosAtr/listadoServiciosAtr">
-			<img src="/views/img/Servicios.png">
+			<img src="/views/img/Listado_Servicios.png">
 			<p>Listado Servicios</p>
 		</a>
 	</div>
 	<div class="listado-submenu">
 		<a href="index.php?action=ServiciosAtr/altaServicioAtr">
-			<img src="/views/img/Puesto.png">
+			<img src="/views/img/Add_Servicio.png">
 			<p>Agregar Servicio</p>
 		</a>
 	</div>
