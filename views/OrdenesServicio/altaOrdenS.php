24c18a82c8285607308047b2620a7c3c021143a9
EdgarMR87 <81342053+EdgarMR87@users.noreply.github.com>
2021-04-30 17:39:10 -0500

    30-04-2021 5:29pm

--- views/OrdenesServicio/altaOrdenS.php
+++ views/OrdenesServicio/altaOrdenS.php
@@ -35,20 +35,62 @@ function ventanaNueva(documento){
 
 //EVENTO DEL BOTON AGREGAR PARTIDA 
 $(document).ready(function(){
-$("#agregar-serv").click(function(){
-    var nFilas = $("#tabla-servicios-pre tr").length;
-    var htmlTags = '<tr>'+
-        '<td>' + nFilas + '<input type="hidden" name="consecutivo[]" value="' + nFilas + '"></td>'+
-        '<td>' + $("#codigo_atr_serv").val() + '<input type="hidden" name="partidas[]" value="'+$("#codigo_atr_serv").val()+'"> </td>'+
-        '<td>' + $("#codigo_atr_serv option:selected").attr('data-name') + '</td>'+
-        '<td>' + $("#observaciones-serv").val() + '<input type="hidden" name="observacionesPartidas[]" value="'+$("#observaciones-serv").val()+'"></td>'+
-        '<td><a class="borrar"><img src="/views/img/eliminar.png" class="img-25"></img></a></td>'+
-       '</tr>';
+   
+    $("#num_orden").blur(function(){
+        var num_orden = $('#num_orden').val();
+        $.ajax({
+            type:'POST',
+            url:  '/views/modules/OrdenesServicio/validarOS',
+            data: {"num_orden" : num_orden}, 
+            dataType: "html",     
+            success: function(resp){
+                if(resp != ""){
+                    alert("Ya existe la OS : " + resp);
+                    $("#num_orden").val('');
+                    $("#num_orden").focus();
+                }
+            }
+        });
+	    return false;  
+    });
+
+    $("#agregar-serv").click(function(){
+        var nFilas = $("#tabla-servicios-pre tr").length;
+        var htmlTags = '<tr>'+
+            '<td>' + nFilas + '<input type="hidden" name="consecutivo[]" value="' + nFilas + '"></td>'+
+            '<td>' + $("#codigo_atr_serv").val() + '<input type="hidden" name="partidas[]" value="'+$("#codigo_atr_serv").val()+'"> </td>'+
+            '<td>' + $("#codigo_atr_serv option:selected").attr('data-name') + '</td>'+
+            '<td>' + $("#observaciones-serv").val() + '<input type="hidden" name="observacionesPartidas[]" value="'+$("#observaciones-serv").val()+'"></td>'+
+            '<td><a class="borrar"><img src="/views/img/eliminar.png" class="img-25"></img></a></td>'+
+           '</tr>';
    $('#tabla-servicios-pre tbody').append(htmlTags);
    $("#observaciones-serv").val('');
    $("#codigo_atr_serv").focus();
    actualizarFilas(); //REORDENAMOS LOS NUMEROS DE CONSECUTIVO
 });
+
+$("#generar").click(function(){
+    var codigos = $("#codigos").val();
+    var array = codigos.split(",");
+    for(var indice in array){
+        var nFilas = $("#tabla-servicios-pre tr").length;
+        var htmlTags = '<tr>'+
+            '<td>' + nFilas + '<input type="hidden" name="consecutivo[]" value="' + nFilas + '"></td>'+
+            '<td>' + array[indice] + '<input type="hidden" name="partidas[]" value="'+ array[indice] +'"> </td>'+
+            '<td>' + $("#codigo_atr_serv option:selected").attr('data-name') + '</td>'+
+            '<td class="td-observ">' + $("#observaciones-serv").val() + '<input type="hidden" name="observacionesPartidas[]" class="observacionesPartidas" value="'+$("#observaciones-serv").val()+'"></td>'+
+            '<td><a class="borrar"><img src="/views/img/eliminar.png" class="img-25"></img></a></td>'+
+           '</tr>';
+   $('#tabla-servicios-pre tbody').append(htmlTags);
+    }
+});
+
+$(document).on('click', '.td-observ', function(event){  
+    $(this).closest('tr').find('input.observacionesPartidas').prop('type','text');
+});
+
+
+
 //POSICIONAR EN COMENTARIOS DESPUES DE SELECCIONAR EL CODIGO 
 
 });
@@ -69,7 +111,7 @@ $(document).on('click', '.borrar', function (event) {
             <tr>
                 <td class="titulo"><p class="derecha">Orden de Servicio : </p></td>
                 <td class=input>
-                    <input type="number" placeholder="# Orden de Servicio" name="num_orden" required>
+                    <input type="number" placeholder="# Orden de Servicio" id="num_orden" name="num_orden" required>
                 </td>
                 <td class="titulo"><p class="derecha">Unidad : </p></td>
                 <td class=input>
@@ -88,7 +130,7 @@ $(document).on('click', '.borrar', function (event) {
                 </td>
                 <td class="titulo"><p class="derecha">Capturo : </p></td>
                 <td class=input>
-                    <select name="captura" id="captura" required> 
+                    <select name="captura" id="captura" required>
                         <option value="" selected disabled>Selecciona un capturista...</option>
                         <option value="ERICK">ERICK</option>
                         <option value="ARMAC">ARMAC</option>
@@ -126,7 +168,15 @@ $(document).on('click', '.borrar', function (event) {
                     <select name="tipo_servicio" id="tipo_servicio"></select>
                 </td>
             </tr>   
+            <tr>
+                <td class="titulo"><p class="derecha">Codigos : </p></td>
+                <td class="input" colspan="2"><input type="text" name="codigos" id="codigos"></td>
+                <td><input class="btn-agregar-serv" type="button" value="generar" name="generar" id="generar"></td>
+            </tr>
     </table>
+
+
+
     <table class="tabla-alta-serv">
         <tr>
             <td class="derecha"><input type="button" value="+" class="btn-modal"  onclick="ventanaNueva('index?action=ServiciosAtr/altaServicioAtr')"></td>
