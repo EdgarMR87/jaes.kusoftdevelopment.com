6566bf10fe04fdfe3b6164491bb515e4ecf82fdb
EdgarMR87 <81342053+EdgarMR87@users.noreply.github.com>
2021-05-07 16:55:05 -0500

    07-05-2021 4:53PM

--- views/template.php
+++ views/template.php
@@ -12,9 +12,62 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.33.1/sweetalert2.all.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.33.1/sweetalert2.css">
-	<link rel="shortcut icon" href="views/img/jaeslogo.png" type="image/png">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.2.0/chart.min.js" integrity="sha512-VMsZqo0ar06BMtg0tPsdgRADvl0kDHpTbugCBBrL55KmucH6hP9zWdLIWY//OTfMnzz6xWQRxQqsUFefwHuHyg==" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js"></script>
+    <link rel="shortcut icon" href="views/img/jaeslogo.png" type="image/png">
+    
     <script type="text/javascript" src="/views/modules/sweetalerts.js"></script>
-	
+	<script>
+    
+  	$(document).ready(function(){
+
+      $("#departamento").on('change', function(){
+			let id_departamento =  document.getElementById('departamento').value;
+    		var dataen = 'id_departamento=' + id_departamento;	
+    		$.ajax({
+        		type:'post',
+        		url:'/views/modules/Usuarios/obtenerPuestosId.php',
+        		data: dataen, 
+        		dataType: "html",     
+        		success: function(resp){      	      
+          		  	$("#puesto").html(resp);
+        		}
+    		});
+    		return false;
+        });
+
+		$("#servicio").on('change', function(){
+			let servicio =  document.getElementById('servicio').value;
+			switch(servicio){
+				case "PREVENTIVO":
+					$('#tipo_servicio').empty();
+					$('#tipo_servicio').prepend("<option value='A' >A</option>");
+					$('#tipo_servicio').prepend("<option value='AB' >AB</option>");
+					$('#tipo_servicio').prepend("<option value='ABC' >ABC</option>");
+					break;
+				default:
+				$('#tipo_servicio').empty();
+				$('#tipo_servicio').prepend("<option value='"+servicio+"'>"+servicio+"</option>");
+			}
+		});
+
+        
+$('#li-usuarios').click(function(event){
+    $('#submenu-usuarios').css('display', 'block');
+    $('#submenu-almacen').css('display', 'none');
+    $('#contenido').css('display', 'none');    
+});
+
+$('#li-almacen').click(function(event){
+    $('#submenu-almacen').css('display', 'block');
+    $('#submenu-usuarios').css('display', 'none');
+    $('#contenido').css('display', 'none');
+});
+
+
+});
+
+</script>
 
 </head>
 
@@ -35,7 +88,7 @@ if(isset($_SESSION["id_departamento"])){
             echo "</section>";
             include "footer.php";
 	    break;
-	    case "1":
+	    case "1" || "17":
 		    if(isset($_SESSION["id_usuario"]))
             $_SESSION["autentificado"] = "SI";
 	        include "views/modules/navegacion.php"; 
@@ -43,7 +96,8 @@ if(isset($_SESSION["id_departamento"])){
 		    $mvc -> enlacesPaginasController();
             echo "</section>";
             include "footer.php";
-	    break;	
+	    break;
+        
     }  
 }else{
     $mvc = new MvcController();
